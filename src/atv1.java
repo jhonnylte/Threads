fc793a4344d4e81d38704f36737e78f7430918d1
Raina Rodrigues <rainarrodrigues@gmail.com>
2024-04-08 22:58:55 -0300

    ädicionando layout textfiel

--- src/atv1.java
+++ src/atv1.java
@@ -1,9 +1,16 @@
 import javax.swing.*;
-import java.awt.Color;
-import java.awt.Dimension;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import static java.awt.AWTEventMulticaster.add;
 
 public class atv1 {
 
+    public JTextField tempoTravessiaLeste;
+    public JTextField tempodePermanciaLeste;
+    public JTextField tempoTravessiaOeste;
+    public JTextField tempodePermanciaOeste;
 
         public static void main(String[] args) {
             int largTela = 800;
@@ -11,6 +18,7 @@ public class atv1 {
 
             int xOeste = 10;
             int xLeste = 750;
+
 
             JFrame frame = new JFrame("Ponte de Carros");
             frame.setSize(new Dimension(largTela, altTela));
@@ -25,6 +33,7 @@ public class atv1 {
             label.setBounds(-1, 150, 800, 198);
             frame.add(label);
 
+
             // Carro
             JPanel carro = new JPanel();
             carro.setBackground(Color.YELLOW);
@@ -33,8 +42,6 @@ public class atv1 {
             frame.add(carro);
             Carro car = new Carro(carro);
             car.start();
-
-
 
             // Carro2
             JPanel carro2 = new JPanel();
@@ -50,20 +57,37 @@ public class atv1 {
             retangulo.setBounds(0, 0, largTela, altTela/2);
             frame.add(retangulo);
 
-
-
             /*   Verificando se a imagem existe
             File file = new File("src/ponte.png");
             System.out.println(file.exists());*/
 
-            // Botões
-            JButton botao1 = new JButton("Adicionar Oeste");
-            botao1.setBounds(largTela-750, altTela-100, 150, 30);
-            frame.add(botao1);
 
-            JButton botao2 = new JButton("Adicionar Leste");
-            botao2.setBounds(largTela-200, altTela-100, 150, 30);
-            frame.add(botao2);
+            //Lado Oeste
+            JTextField tempoTravessiaOeste = new JTextField(10);
+            tempoTravessiaOeste.setBounds(largTela-750, altTela-180, 150, 30);
+            frame.add(tempoTravessiaOeste);
+
+            JTextField tempodePermanciaOeste = new JTextField(10);
+            tempodePermanciaOeste.setBounds(largTela-750, altTela-140, 150, 30);
+            frame.add(tempodePermanciaOeste);
+
+            JButton botaoOeste = new JButton("Adicionar Oeste");
+            botaoOeste.setBounds(largTela-750, altTela-100, 150, 30);
+            frame.add(botaoOeste);
+
+
+            //Lado Leste
+            JTextField tempoTravessiaLeste = new JTextField(10);
+            tempoTravessiaLeste.setBounds(largTela-200, altTela-180, 150, 30);
+            frame.add(tempoTravessiaLeste);
+
+            JTextField tempodePermanciaLeste = new JTextField(10);
+            tempodePermanciaLeste.setBounds(largTela-200, altTela-140, 150, 30);
+            frame.add(tempodePermanciaLeste);
+
+            JButton botaoLeste = new JButton("Adicionar Leste");
+            botaoLeste.setBounds(largTela-200, altTela-100, 150, 30);
+            frame.add(botaoLeste);
 
             frame.setVisible(true);
 
