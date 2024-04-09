2b0bfaa9d6eb4b00530e866b8203775ee5b9d5d3
Jhonny <jhonnyleite.14@gmail.com>
2024-04-09 17:49:04 -0300

    Ida e volta

--- src/Carro.java
+++ src/Carro.java
@@ -3,23 +3,21 @@ import java.awt.*;
 
 public class Carro extends Thread{
     public JPanel panel = new JPanel();
-    public int Xfinal;
-    public int Xinicio;
-    public int speed = 0;
+    public int Xfinal = 800;
+    public int Xinicio = 0;
+    public int passo = 0;
     public enum origem {oeste, leste}
     public origem o;
     public Carro(origem o) {
         this.o = o;
         if(this.o == origem.oeste) {
-            speed = 10;
-            Xfinal = 720;
+            passo = 10;
             this.panel.setBackground(Color.YELLOW);
             this.panel.setPreferredSize(new Dimension(10, 10));
             this.panel.setBounds(10, 230, 30, 15);
 
         } else if(this.o == origem.leste){
-            speed = 10;
-            Xinicio = 10;
+            passo = 10;
             this.panel.setBackground(Color.BLUE);
             this.panel.setPreferredSize(new Dimension(10, 10));
             this.panel.setBounds(750, 230, 30, 15);
@@ -30,22 +28,56 @@ public class Carro extends Thread{
         int currentX = panel.getX();
 
         try {
-            if(this.o == origem.oeste)
-                while (panel.getX() < Xfinal){
-                    currentX += speed;
+            if(this.o == origem.oeste) {
+                // IDA
+                while (panel.getX() < Xfinal) {
+                    currentX += passo;
                     int finalCurrentX = currentX;
                     SwingUtilities.invokeLater(() -> panel.setLocation(finalCurrentX, panel.getY()));
                     System.out.println(panel.getX());
-                    Thread.sleep(250);
+
+                    // Velocidade
+                    Thread.sleep(10);
+
                 }
-            if(this.o == origem.leste)
-                while (panel.getX() > Xinicio){
-                    currentX -= speed;
+
+                // VOLTA
+                while (panel.getX() > Xinicio) {
+                    currentX -= passo;
                     int finalCurrentX = currentX;
                     SwingUtilities.invokeLater(() -> panel.setLocation(finalCurrentX, panel.getY()));
                     System.out.println(panel.getX());
-                    Thread.sleep(250);
+
+                    // Velocidade
+                    Thread.sleep(10);
                 }
+
+            }
+
+            if(this.o == origem.leste) {
+                // IDA
+                while (panel.getX() > Xinicio) {
+                    currentX -= passo;
+                    int finalCurrentX = currentX;
+                    SwingUtilities.invokeLater(() -> panel.setLocation(finalCurrentX, panel.getY()));
+                    System.out.println(panel.getX());
+
+                    // Velocidade
+                    Thread.sleep(10);
+                }
+
+                // VOLTA
+                while (panel.getX() < Xfinal) {
+                    currentX += passo;
+                    int finalCurrentX = currentX;
+                    SwingUtilities.invokeLater(() -> panel.setLocation(finalCurrentX, panel.getY()));
+                    System.out.println(panel.getX());
+
+                    // Velocidade
+                    Thread.sleep(10);
+
+                }
+            }
 
 
         } catch (InterruptedException e) {
